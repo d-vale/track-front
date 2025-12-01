6e92ae97d34e19bce05720aaeef250bd4ae81fba
Joé Favre <joefa@MacBook-Pro-de-Joe.local>
2025-12-01 09:59:11 +0100

    created routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue';
 import Tracking from '../views/Tracking.vue';
 import Profile from '../views/Profile.vue';
+import Home from '../views/Home.vue';
+import ActivitiesHistory from '../views/ActivitiesHistory.vue';
+import ActivityDetail from '../views/ActivityDetail.vue';
 
 const routes = [
   {
@@ -22,6 +25,21 @@ const routes = [
     path: '/profile',
     name: 'Profile',
     component: Profile
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/history',
+    name: 'History',
+    component: ActivitiesHistory
+  },
+  {
+    path: '/activity:id',
+    name: 'ActivityDetail',
+    component: ActivityDetail
   }
 ];
 
