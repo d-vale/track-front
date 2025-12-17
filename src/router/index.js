70ccf6a05928e1d6e59927f0e12c3c5555db11b7
Jonathan <jpinard@bluewin.ch>
2025-12-17 12:18:38 +0100

    fix: web hash historx

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Login from '../views/Login.vue';
 import Tracking from '../views/Tracking.vue';
 import Profile from '../views/Profile.vue';
@@ -44,7 +44,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
